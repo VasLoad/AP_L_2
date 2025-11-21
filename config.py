32ebeafc4dac5d4d52b808e7a41ccffba124940d
SoTBeR <Vasyustenko@gmail.com>
2025-11-21 03:53:02 +0300

    Обновлён README.md

--- config.py
+++ config.py
@@ -6,6 +6,6 @@
 
 # HYPERLINK_PATTERN = r"<a\s+[^>]*?\bhref\s*=\s*(?P<border>[\"'])(?P<url>[^\"']+)(?P=border)[^>]*?>" # re.DOTALL без контента
 
-# HYPERLINK_PATTERN = r"<a\s+(?:[^>]*?\s)?\bhref\s*=\s*(?P<border>[\"'])(?P<url>[^\"']*)(?P=border)[^>]*?>(?P<content>.*?)<\/a>" # re.DOTALL для что-то-href
+# HYPERLINK_PATTERN = r"<a\s+(?:[^>]*?\s)?\bhref\s*=\s*(?P<border>[\"'])(?P<url>[^\"']*)(?P=border)[^>]*>(?P<content>.*?)<\/a>" # re.DOTALL для что-то-href
 
-HYPERLINK_PATTERN = r"<a\s+(?:[^>]*?\s)?\bhref\s*=\s*(?P<border>[\"'])(?P<url>[^\"']*)(?P=border)[^>]*?>" # re.DOTALL без контента для что-то-href
+HYPERLINK_PATTERN = r"<a\s+(?:[^>]*?\s)?\bhref\s*=\s*(?P<border>[\"'])(?P<url>[^\"']*)(?P=border)[^>]*>" # re.DOTALL без контента для что-то-href
